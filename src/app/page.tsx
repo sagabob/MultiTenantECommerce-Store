f329e3bdad16aae3453cbaa54770890c26bd23de
Bob Pham <bobpham.tdp@gmail.com>
2025-07-26 15:57:31 +1000

    Start using tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <div>
-      <button>Hello World</button>
+      <Button variant={"destructive"}>Hello World</Button>
     </div>
   );
 }
