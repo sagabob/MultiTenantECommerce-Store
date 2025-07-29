1f224097862cc5960b1e654fbe473e749f8b5ef9
Bob Pham <bobpham.tdp@gmail.com>
2025-07-30 00:29:30 +1000

    add style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 
 export default function Home() {
   return (
-    <div>
-      <Button variant={"destructive"}>Hello World</Button>
+    <div >
+      <div className="flex flex-col gap-y-4  max-w-md mx-auto">
+        <div>
+          <Button >Hello World</Button>
+        </div>
+        <div>
+          <Input placeholder="Enter your name" />
+        </div>
+      </div>
     </div>
   );
 }
